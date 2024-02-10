09509ad486e906ae6f4c3bf81eaefee3b87c67b3
utsav <sav.acharya22@gmail.com>
2024-02-10 23:36:38 +1100

    create cli controller

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
