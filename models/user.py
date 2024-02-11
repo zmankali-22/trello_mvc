6fc48ce107d8b576dfa0244f36b6fa7b8326b4a0
utsav <sav.acharya22@gmail.com>
2024-02-11 12:11:19 +1100

    register and login function

--- models/user.py
+++ models/user.py
@@ -6,9 +6,9 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 class UserSchema(ma.Schema):
