cbd19a15b1d41b1d4027dea16040d094c8471c72
utsav <sav.acharya22@gmail.com>
2024-02-24 20:06:30 +1100

    delete all commnets on acard

--- models/card.py
+++ models/card.py
@@ -14,7 +14,7 @@ class Card(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     user = db.relationship('User', back_populates='cards')
-    comments = db.relationship('Comment', back_populates='card', cascade='all, delete')
+    comments = db.relationship('Comment', back_populates='card', cascade= 'all, delete')
 
 class CardSchema(ma.Schema):
     user = fields.Nested('UserSchema', only = ['name', 'email'])
